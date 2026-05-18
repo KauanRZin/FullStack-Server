5512dd2d173a73bfd2595d92f7e716a846913bc3
Kauan <Kauan.kr1001@gmail.com>
2026-05-18 17:04:31 -0300

    feat: update server configuration and add model files for category, order, product, and user

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const prisma = require('./database/prisma.js')
 
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT 
 
 app.use(cors())
 app.use(express.json())
